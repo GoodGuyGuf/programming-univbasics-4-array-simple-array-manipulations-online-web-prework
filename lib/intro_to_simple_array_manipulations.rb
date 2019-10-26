12d999a25106bff0a32ef9e951825a1ce2ca7680
Ethan <ethan_gustafson@outlook.com>
2019-10-26 18:47:35 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,54 @@
+  def using_push
+    colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
+    next_color = "violet"
+    colors_in_the_rainbow.push(next_color)
+    colors_in_the_rainbow
+  end
+  
+  def using_unshift
+    bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
+     new_neighborhood = "Staten Island"
+     bouroughs_in_nyc.unshift(new_neighborhood)
+     bouroughs_in_nyc
+  end
+  
+  def using_pop
+    
+  end
+  
+  def pop_with_args
+    
+  end
+  
+  def using_unshift
+    
+end
+
+def shift_with_args
+  
+end
+
+def using_concat
+  
+end
+
+def using_insert
+  
+end
+
+def using_uniq
+  
+end
+
+def using_flatten
+  
+end
+
+def using_delete
+  
+end
+
+def using_delete_at
+  
+end
+
