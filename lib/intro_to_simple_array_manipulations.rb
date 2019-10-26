4e57b861dd389830528cdb2e5686529934d6f8a5
Ethan <ethan_gustafson@outlook.com>
2019-10-26 19:02:11 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,53 +1,48 @@
-  def using_push
-    colors_in_the_rainbow
-    colors_in_the_rainbow.push(next_color)
-    colors_in_the_rainbow
-  end
-  
-  def using_unshift
-    bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
-     new_neighborhood = "Staten Island"
-     bouroughs_in_nyc.unshift(new_neighborhood)
-     bouroughs_in_nyc
-  end
-  
-  def using_pop
-    
-  end
-  
-  def pop_with_args
-    
-  end
-  
-  def using_unshift
-    
+def using_push (colors_in_the_rainbow, next_color)
+  colors_in_the_rainbow.push(next_color)
 end
 
-def shift_with_args
-  
+def using_unshift (bouroughs_in_nyc, next_color)
+  bouroughs_in_nyc.unshift(next_color)
 end
 
-def using_concat
-  
+def using_pop (continents)
+  deleted_string = continents.pop
 end
 
-def using_insert
-  
+def pop_with_args(dog_breeds)
+  small_dogs = dog_breeds.pop(2)
 end
 
-def using_uniq
-  
+def using_shift(my_favorite_cities)
+  im_so_over_this_city = my_favorite_cities.shift
 end
 
-def using_flatten
-  
+
+def shift_with_args(ice_cream_brands)
+  brands_removed = ice_cream_brands.shift(2)
 end
 
-def using_delete
-  
+def using_concat(my_favorite_things, more_favs)
+  my_favorite_things.concat(more_favs)
 end
 
-def using_delete_at
-  
+def using_insert (list_of_programming_languages, another_language)
+  list_of_programming_languages.insert(4, another_language)
 end
 
+def using_uniq (haircuts)
+  haircuts.uniq
+end
+
+def using_flatten (instruments)
+  instruments.flatten
+end
+
+def using_delete (instructors, no_offense_steven)
+  instructors.delete("Steven")
+end
+
+def using_delete_at (famous_robots, deleted_robot)
+  famous_robots.delete_at(2)
+end
